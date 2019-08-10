6323241b4345f38d9684463aa71db033a006c7bc
codewnw <code.wnw@gmail.com>
2019-08-10 10:32:00 +0530

    Servlet introduction

---
+++ src/com/jm2190615/servlet/HelloServlet.java
@@ -0,0 +1,21 @@
+package com.jm2190615.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("I'm running....");
+		
+		PrintWriter out = resp.getWriter();
+		out.print("Hi, how are you?");
+	}
+
+}
